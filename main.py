dda8ecf00d7cd940dea6d208e20894c58812fc30
SydneyARCHT <sstiward@hotmail.com>
2024-06-24 14:37:26 -0700

    First Commit / Lesson 1 Modules Homework

---
+++ main.py
@@ -0,0 +1,5 @@
+# Lesson 1: Assignments | Modules
+
+import mood_responses
+mood = input("How we feeling today fella? ")
+print(mood_responses.mood_response(mood))
---
+++ mood_responses.py
@@ -0,0 +1,12 @@
+# Lesson 1: Assignments | Modules
+
+def mood_response(mood):
+    if mood == "happy":
+        return "Great! It's going to be a great day! "
+    elif mood == "sad":
+        return "Let's turn that frown upside down! "
+    elif mood == "excited":
+        return "On top of the world, are we? "
+    else:
+        return "Haven't heard of that mood... "
+    
